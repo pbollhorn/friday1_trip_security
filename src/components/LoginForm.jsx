89a82e8401a3ea00ac912a3ef8b2daaea3487d64
Peter Bollhorn <pbollhorn@gmail.com>
2025-05-11 14:21:46 +0200

    can now login and logout but DOM does not update automatically

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -5,18 +5,32 @@ export default function LoginForm() {
   const usernameRef = useRef(null);
   const passwordRef = useRef(null);
 
-  const handleSubmit = (e) => {
+  const handleLoginSubmit = (e) => {
     e.preventDefault();
     api.login(usernameRef.current.value, passwordRef.current.value);
   };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>Username: </label>
-      <input type="text" ref={usernameRef} />
-      <label> Password: </label>
-      <input type="password" ref={passwordRef} />{" "}
-      <button type="submit">Login</button>
-    </form>
-  );
+  const handleLogoutSubmit = (e) => {
+    e.preventDefault();
+    api.logout();
+  };
+
+  if (api.loggedIn() === false) {
+    return (
+      <form onSubmit={handleLoginSubmit}>
+        <label>Username: </label>
+        <input type="text" ref={usernameRef} />
+        <label> Password: </label>
+        <input type="password" ref={passwordRef} />{" "}
+        <button type="submit">Login</button>
+      </form>
+    );
+  }
+  if (api.loggedIn() === true) {
+    return (
+      <form onSubmit={handleLogoutSubmit}>
+        <button type="submit">Logout</button>
+      </form>
+    );
+  }
 }
