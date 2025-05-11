9165a49a4719ed7e9fe51749ff488ed872c37ac4
Peter Bollhorn <pbollhorn@gmail.com>
2025-05-11 17:20:15 +0200

    progression

--- src/components/Guides.jsx
+++ src/components/Guides.jsx
@@ -6,11 +6,13 @@ export default function Guides() {
 
   // useEffect
   useEffect(() => {
-    const fun = async () => {
-      setGuideData(await fetchGuideData());
-    };
+    if (api.loggedIn()) {
+      const fun = async () => {
+        setGuideData(await fetchGuideData());
+      };
 
-    fun();
+      fun();
+    }
   }, []); // Empty dependency array means this runs once on mount
 
   return (
@@ -47,3 +49,20 @@ async function fetchGuideData() {
 
   return guideData;
 }
+
+// Check for user access. Move to another file.
+const getUserRoles = () => {
+  const token = getToken();
+  if (token != null) {
+    const payloadBase64 = getToken().split(".")[1];
+    const decodedClaims = JSON.parse(window.atob(payloadBase64));
+    const roles = decodedClaims.roles;
+    return roles;
+  } else return "";
+};
+
+// Check for user access. Move to another file.
+const hasUserAccess = (neededRole, loggedIn) => {
+  const roles = getUserRoles().split(",");
+  return loggedIn && roles.includes(neededRole);
+};
