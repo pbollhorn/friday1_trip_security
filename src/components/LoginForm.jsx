77333477f25790cb322134948b4313fcbf0aefe3
Peter Bollhorn <pbollhorn@gmail.com>
2025-05-11 13:53:54 +0200

    login form now takes email and password

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,25 +1,21 @@
 import React, { useRef } from "react";
 
 export default function LoginForm() {
-  const nameRef = useRef(null);
   const emailRef = useRef(null);
+  const passwordRef = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Name:", nameRef.current.value);
     console.log("Email:", emailRef.current.value);
+    console.log("Password:", passwordRef.current.value);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-
-        <label>Name: </label>
-        <input type="text" ref={nameRef} />
-
-
-        <label>Email: </label>
-        <input type="email" ref={emailRef} />
-
+      <label>Email: </label>
+      <input type="email" ref={emailRef} />
+      <label> Password: </label>
+      <input type="password" ref={passwordRef} />{" "}
       <button type="submit">Login</button>
     </form>
   );
