09077379702145f1193782c4ae964eaccf05cef6
Peter Bollhorn <pbollhorn@gmail.com>
2025-05-11 17:04:35 +0200

    can now show guide list

--- src/components/Guides.jsx
+++ src/components/Guides.jsx
@@ -1,7 +1,49 @@
+import React, { useState, useEffect } from "react";
+import api from "../apiFacade.js";
+
 export default function Guides() {
+  const [guideData, setGuideData] = useState([]);
+
+  // useEffect
+  useEffect(() => {
+    const fun = async () => {
+      setGuideData(await fetchGuideData());
+    };
+
+    fun();
+  }, []); // Empty dependency array means this runs once on mount
+
   return (
     <>
       <h2>Guides</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {guideData.map((e) => (
+            <tr key={e.id}>
+              <td>{e.firstname}</td>
+              <td>{e.lastname}</td>
+              <td>{e.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
+}
+
+async function fetchGuideData() {
+  const response = await fetch(
+    "https://tripapi.cphbusinessapps.dk/api/guides",
+    api.makeOptions("GET", true)
+  );
+  const guideData = await response.json();
+
+  return guideData;
 }
